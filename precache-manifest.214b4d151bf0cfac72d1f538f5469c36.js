b3cdf91df8166fd56efa106e567d4208b18a43f4
Roy Dey <roydey@ucdavis.edu>
2020-11-07 21:18:09 -0800

    Updates

---
+++ precache-manifest.214b4d151bf0cfac72d1f538f5469c36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8e0168e3765547837d9",
+    "url": "/raphunt/static/css/main.4a0782c9.chunk.css"
+  },
+  {
+    "revision": "d8e0168e3765547837d9",
+    "url": "/raphunt/static/js/main.d8e0168e.chunk.js"
+  },
+  {
+    "revision": "fd62478069438890a8e0",
+    "url": "/raphunt/static/js/1.fd624780.chunk.js"
+  },
+  {
+    "revision": "da119faac6853c5e86d4",
+    "url": "/raphunt/static/js/runtime~main.da119faa.js"
+  },
+  {
+    "revision": "3ece22232a49039e9b888150dd1086da",
+    "url": "/raphunt/static/media/fire.3ece2223.png"
+  },
+  {
+    "revision": "4c091063885322c694d796680a6b748d",
+    "url": "/raphunt/index.html"
+  }
+];
